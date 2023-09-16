b9298e8f976d3a0725a92edd725ea01d4a0e5ac4
Jacob Roush <roush.jake@gmail.com>
2023-09-16 15:26:35 -0400

    showing something but very broken

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+import MapComponent from './components/MapComponent';
+
+function App() {
+  return (
+    <MapComponent />
+  )
+}
+
+export default App;
---
+++ src/components/MapComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { mapState } from '../state/mapState';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import '../App.css'
+
+
+const MapComponent: React.FC = () => {
+  const [mapData, setMapData] = useRecoilState(mapState);
+
+  return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  </MapContainer>
+};
+
+export default MapComponent;
