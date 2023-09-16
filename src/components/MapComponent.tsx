22c130df28e49c33a5642940b59c2b80b6b86cd0
Jacob Roush <roush.jake@gmail.com>
2023-09-16 16:15:51 -0400

    change map center

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -8,7 +8,7 @@ import '../App.css'
 const MapComponent: React.FC = () => {
   const [mapData, setMapData] = useRecoilState(mapState);
 
-  return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  return <MapContainer center={[40.505, -80.09]} zoom={13} scrollWheelZoom={false}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
