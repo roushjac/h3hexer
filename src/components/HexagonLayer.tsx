2c9b17fa7b4dbfb605ae117b575ddab091d9dec4
Jacob Roush <roush.jake@gmail.com>
2023-09-19 17:44:33 -0400

    automatically zoom to hex bounds

--- src/components/HexagonLayer.tsx
+++ src/components/HexagonLayer.tsx
@@ -3,16 +3,37 @@ import { Polygon } from 'react-leaflet';
 import { useRecoilValue } from 'recoil';
 import { CoordPair } from 'h3-js';
 import { hexPolygonsState } from '../state/hexPolygonsState';
+import { h3ToGeoJSON } from '../utils/h3';
+import { useMap } from 'react-leaflet';
+import { useEffect } from 'react';
+import L from 'leaflet';
 
 const HexagonLayer: React.FC = () => {
-    const hexFeatures = useRecoilValue(hexPolygonsState);
+    const hexMultipolygonCoords = useRecoilValue(hexPolygonsState);
+
+    const map = useMap();
+
+    useEffect(() => {
+        if (hexMultipolygonCoords) {
+            // convert multipolygon coordinates into valid GeoJSON
+            const hexFeatures = h3ToGeoJSON(hexMultipolygonCoords);
+            console.log(hexFeatures);
+            const geoJsonLayer = L.geoJSON(hexFeatures); // Create a temporary Leaflet GeoJSON layer
+            const bounds = geoJsonLayer.getBounds(); // Get bounds of the GeoJSON layer
+            if (bounds.isValid()) {
+                map.fitBounds(bounds); // Fit map to bounds
+            }
+        }
+    }, [hexMultipolygonCoords, map]);
 
     return (
         <>
-            {hexFeatures &&
-                hexFeatures.map((feature: CoordPair[][], index: number) => (
-                    <Polygon key={index} positions={feature} color="blue" />
-                ))}
+            {hexMultipolygonCoords &&
+                hexMultipolygonCoords.map(
+                    (feature: CoordPair[][], index: number) => (
+                        <Polygon key={index} positions={feature} color="blue" />
+                    )
+                )}
         </>
     );
 };
--- src/utils/h3.ts
+++ src/utils/h3.ts
@@ -1,11 +1,10 @@
 import * as h3 from 'h3-js';
+import { Feature, FeatureCollection, MultiPolygon } from 'geojson';
 
 export const h3PolyfillGeoJSON = (geoJSONString: string) => {
     const geoJSON = JSON.parse(geoJSONString);
     const resolution = 9;
     let allH3Indexes: string[] = [];
-
-    console.log('geoJSON', geoJSON);
 
     geoJSON.features.forEach((feature: any) => {
         const coordinates = feature.geometry.coordinates;
@@ -17,12 +16,43 @@ export const h3PolyfillGeoJSON = (geoJSONString: string) => {
         allH3Indexes = [...allH3Indexes, ...oneFeatureIndexes];
     });
 
-    console.log('indexes', allH3Indexes);
-
-    // Convert H3 hexagons to GeoJSON
+    // Convert H3 hexagons to multipolygon coordinates
     const h3Polygons = h3.cellsToMultiPolygon(allH3Indexes, false);
 
-    console.log('h3Polygons', h3Polygons);
-
     return h3Polygons;
+};
+
+type CoordPair = [number, number]; // Latitude, Longitude
+type h3CoordSet = CoordPair[][]; // Multiple polygons, each with multiple coordinates
+
+/**
+ * Convert h3.CoordPair[][][] to GeoJSON MultiPolygon FeatureCollection
+ * @param {h3CoordSet[]} h3Coords - List of h3CoordSets
+ * @returns {FeatureCollection<MultiPolygon>} - GeoJSON MultiPolygon FeatureCollection
+ */
+export const h3ToGeoJSON = (
+    h3Coords: h3CoordSet[]
+): FeatureCollection<MultiPolygon> => {
+    const features: Array<Feature<MultiPolygon>> = h3Coords.map(
+        (polygon, index) => {
+            // Flip lat and lon for each vertex. Need this because h3 uses lat/lon by default, geojson needs lon/lat
+            const flippedPolygon = polygon.map((ring) =>
+                ring.map((coordPair) => [coordPair[1], coordPair[0]])
+            );
+
+            return {
+                type: 'Feature',
+                properties: { id: index },
+                geometry: {
+                    type: 'MultiPolygon',
+                    coordinates: [flippedPolygon] // Wrap it in an additional array to comply with the MultiPolygon structure
+                }
+            };
+        }
+    );
+
+    return {
+        type: 'FeatureCollection',
+        features
+    };
 };
