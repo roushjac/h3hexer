ff4eb05fc22b0bfb1c7d7813b85b421790d104fa
Jacob Roush <roush.jake@gmail.com>
2023-09-19 16:17:30 -0400

    remove leaflet controls, make tools relative position

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { hexPolygonsState } from './state/hexPolygonsState';
 import { h3PolyfillGeoJSON } from './utils/h3';
 import HexagonLayer from './components/HexagonLayer';
 import ToolComponents from './components/ControlsContainer';
+import './App.css';
 
 const App: React.FC = () => {
     const fileContent = useRecoilValue(fileContentState);
@@ -20,19 +21,21 @@ const App: React.FC = () => {
     }, [fileContent]);
 
     return (
-        <MapContainer
-            center={[38.505, -95.09]}
-            zoom={5}
-            scrollWheelZoom={true}
-            style={{ minHeight: '100vh', minWidth: '100vw' }}
-        >
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <ToolComponents />
-            {hexData && <HexagonLayer hexagonData={hexData} />}
-        </MapContainer>
+        <div className="map-wrapper">
+            <MapContainer
+                center={[38.505, -95.09]}
+                zoom={5}
+                scrollWheelZoom={true}
+                style={{ minHeight: '100vh', minWidth: '100vw' }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <ToolComponents />
+                {hexData && <HexagonLayer hexagonData={hexData} />}
+            </MapContainer>
+        </div>
     );
 };
 
--- src/components/ControlsContainer.tsx
+++ src/components/ControlsContainer.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-import { useMap } from 'react-leaflet';
-import L from 'leaflet';
+import React from 'react';
 import FileUploader from './FileUploader';
 import '../styles/ControlsContainer.css';
 
@@ -9,41 +6,13 @@ const ControlsContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
     return <div className="container">{children}</div>;
 };
 
-const createControl = (components: React.ReactNode) => {
-    return L.Control.extend({
-        onAdd: function () {
-            const div = L.DomUtil.create('div', '');
-            const root = createRoot(div);
-            root.render(components);
-            return div;
-        }
-    });
-};
-
 const ToolComponents: React.FC = () => {
-    const map = useMap();
-    let control: any = null;
-
-    useEffect(() => {
-        const components = (
-            <ControlsContainer>
-                <FileUploader />
-                {/* Add other components here */}
-            </ControlsContainer>
-        );
-
-        const LeafletControl = createControl(components);
-        control = new LeafletControl();
-        control.addTo(map);
-
-        return () => {
-            if (control && map) {
-                map.removeControl(control);
-            }
-        };
-    }, [map]);
-
-    return null; // Return null since this component doesn't render anything itself
+    return (
+        <ControlsContainer>
+            <FileUploader />
+            {/* Add other components here */}
+        </ControlsContainer>
+    );
 };
 
 export default ToolComponents;
