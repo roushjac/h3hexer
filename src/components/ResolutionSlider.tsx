3b0fa4624836d682eacd3ae0f4b1e34e699298a5
Jacob Roush <roush.jake@gmail.com>
2023-10-26 13:13:08 -0400

    disable map panning while using res slider

--- src/components/ResolutionSlider.tsx
+++ src/components/ResolutionSlider.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import { Slider } from 'antd';
+import { useMap } from 'react-leaflet';
 import { hexResolutionState } from '../state/hexResolutionState';
 import '../styles/ResolutionSlider.css';
 
 const ResolutionSlider: React.FC = () => {
     const [hexResolution, setHexResolution] = useRecoilState(hexResolutionState);
 
+    const map = useMap();
+
     const handleChange = (value: number) => {
         setHexResolution(value);
     };
 
     return (
-        <div className="slider-container">
+        <div
+            className="slider-container"
+            onMouseEnter={() => map.dragging.disable()}
+            onMouseLeave={() => map.dragging.enable()}
+        >
             <span className="slider-title">H3 resolution:</span>
             <Slider min={0} max={15} value={hexResolution} onChange={handleChange} className="resolution-slider" />
         </div>
